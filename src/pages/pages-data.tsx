ae62e6f58e16ada7bc2b47d94b61553e21a48db8
Edson Costa <edson.13.10.2004@gmail.com>
2024-09-23 22:57:26 -0300

    ajust

--- src/pages/pages-data.tsx
+++ src/pages/pages-data.tsx
@@ -1,5 +1,5 @@
 import { routerType } from "../utilities";
-import Home from "./home";
+import { Home } from "./home";
 
 const pagesData: routerType[] = [{ path: "", element: <Home />, title: "" }];
 

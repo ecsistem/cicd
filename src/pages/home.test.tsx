f72a3dcc2fded981473e6a43b15aa6bcdcbb8e9d
Edson Costa <edson.13.10.2004@gmail.com>
2024-09-23 22:58:41 -0300

    teste

--- src/pages/home.test.tsx
+++ src/pages/home.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
-import { Home } from './home';
+import Home from './home';
 
 describe('Home Component', () => {
   test('renders without crashing', () => {
